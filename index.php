236c2db91af60f8e6043f50bec019487d129b457
Bataino Ronaldo <Bataino.ronaldo@gmail.com>
2020-12-13 15:18:34 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,22 +4,6 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
   <body onload="load()">
-    <!-- ClickDesk Live Chat Service for websites -->
-      <script type='text/javascript'>
-      var _glc =_glc || []; _glc.push('all_ag9zfmNsaWNrZGVza2NoYXRyEgsSBXVzZXJzGICAiKvX78cKDA');
-      if(document.location.hostname != 'localhost'){
-        var glcpath = (('https:' == document.location.protocol) ? 'https://my.clickdesk.com/clickdesk-ui/browser/' : 
-        'http://my.clickdesk.com/clickdesk-ui/browser/');
-        var glcp = (('https:' == document.location.protocol) ? 'https://' : 'http://');
-      }else{
-        var glcpath = 'http://localhost:8888/clickdesk-ui/browser/';
-        var glcp = 'http://';
-      }
-      var glcspt = document.createElement('script'); glcspt.type = 'text/javascript'; 
-      glcspt.async = true; glcspt.src = glcpath + 'livechat-cloud-new.js';
-      var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(glcspt, s);
-      </script>
-      <!-- End of ClickDesk -->
     <script>
       function load(){
         location.href = '#mobile-widget';
