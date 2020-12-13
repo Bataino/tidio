8d1ba0d287d682f116911adbfb3b4d6bfc1248d4
Bataino Ronaldo <Bataino.ronaldo@gmail.com>
2020-12-13 15:48:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,6 @@
         location.href = '#mobile-widget';
       }
     </script>
-    <script srcgndhgcfth="https://widget-v4.tidiochat.com//1_45_0/static/js/widget.66f097c8c651f346d9e7.js" async></script>
+    <script src="https://widget-v4.tidiochat.com//1_45_0/static/js/widget.66f097c8c651f346d9e7.js" async></script>
   </body>
 </html>
