97253f34009e0da6d74cb8e31eee4a5ddebc2339
Bataino Ronaldo <Bataino.ronaldo@gmail.com>
2020-12-13 15:36:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
   <body onload="load()">
-    <script src="//code.tidio.co/iunyqqxfscfakenjcreqr2qehdtozzi4.js" async></script>
     <script>
       function load(){
         location.href = '#mobile-widget';
       }
     </script>
+    <script src="//code.tidio.co/iunyqqxfscfakenjcreqr2qehdtozzi4.js" async></script>
   </body>
 </html>
