5f9b84168d78df6977cd6aba9a63ad6f7d56ec8d
Bataino Ronaldo <Bataino.ronaldo@gmail.com>
2020-12-13 14:44:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,23 @@
-
-<script src="//code.tidio.co/iunyqqxfscfakenjcreqr2qehdtozzi4.js" async></script>
+<html>
+  <head>
+          <!-- ClickDesk Live Chat Service for websites -->
+      <script type='text/javascript'>
+      var _glc =_glc || []; _glc.push('all_ag9zfmNsaWNrZGVza2NoYXRyEgsSBXVzZXJzGICAiKvX78cKDA');
+      if(document.location.hostname != 'localhost'){
+        var glcpath = (('https:' == document.location.protocol) ? 'https://my.clickdesk.com/clickdesk-ui/browser/' : 
+        'http://my.clickdesk.com/clickdesk-ui/browser/');
+        var glcp = (('https:' == document.location.protocol) ? 'https://' : 'http://');
+      }else{
+        var glcpath = 'http://localhost:8888/clickdesk-ui/browser/';
+        var glcp = 'http://';
+      }
+      var glcspt = document.createElement('script'); glcspt.type = 'text/javascript'; 
+      glcspt.async = true; glcspt.src = glcpath + 'livechat-cloud-new.js';
+      var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(glcspt, s);
+      </script>
+      <!-- End of ClickDesk -->
+      <script src="//code.tidio.co/iunyqqxfscfakenjcreqr2qehdtozzi4.js" async></script>
+  </head>
+  <body>
+  </body>
+</html>
